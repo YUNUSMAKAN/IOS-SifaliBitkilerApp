5e350833f330caa78224e35d78896c1af17216cb
Yunus MAKAN <makanyunus@gmail.com>
2020-04-09 16:58:22 +0300

    Initial Commit

---
+++ SifaliBitkiler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SifaliBitkiler
+//
+//  Created by MAKAN on 9.04.2020.
+//  Copyright © 2020 YUNUS MAKAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
